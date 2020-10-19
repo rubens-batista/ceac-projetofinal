554904174389064c1d436ec4f40f0b1f5cfb2e42
Rubens Batista <nunobfc24@gmail.com>
2020-10-19 22:25:51 +0100

    login

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
     <!--Navbar-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top"><!--Nav-->
         <div class="container"><!--Nav Container-->
-            <a class="navbar-brand" href="index.html" style="max-width: 7em;">
+            <a class="navbar-brand" href="index.php" style="max-width: 7em;">
                 <img src="img/logo-dark.png" alt="Logo Marca" class="img-fluid">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,7 +37,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent"><!--Collapse Nav-->
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="about.html">About</a>
@@ -59,7 +59,7 @@
                   </div>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">FAQ</a>
+                  <a class="nav-link" href="login.php">Login</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="contact.html">Contact</a>
